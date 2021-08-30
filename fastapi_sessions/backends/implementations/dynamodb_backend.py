5e10a9d76bcb0b174a98823d42041e688751cf65
Derrick Petzold <dpetzold@shopstyle.com>
2021-08-30 15:44:08 -0700

    first pass

--- fastapi_sessions/backends/implementations/dynamodb_backend.py
+++ fastapi_sessions/backends/implementations/dynamodb_backend.py
@@ -1,6 +1,8 @@
 """InMemoryBackend implementation."""
 import boto3
-from typing import Dict, Generic
+from boto3.dynamodb.conditions import Key
+
+from typing import Generic
 
 from config import settings
 
@@ -26,32 +28,40 @@ class DynamoDbBackend(Generic[ID, SessionModel], SessionBackend[ID, SessionModel
     def dynamodb_client(self):
         return self.aws_client("dynamodb")
 
-    def __init__(self) -> None:
-        """Initialize a new in-memory database."""
-        self.data: Dict[ID, SessionModel] = {}
+    @property
+    def resource(self):
+        return boto3.resource("dynamodb", region_name=settings.get("AWS_REGION"))
+
+    @property
+    def table(self):
+        return self.resource.Table(self.table_name)
+
+    def get(self, session_id):
+        return self.table.query(
+            KeyConditionExpression=(Key("session_id").eq(session_id)),
+        )["Items"]
+
+    def put(self, session_id, data: SessionModel = None):
+        item = data.dict() if data else {}
+        item.update({"SessionId": session_id})
+        self.table.put_item(Item=item)
 
     async def create(self, session_id: ID, data: SessionModel):
         """Create a new session entry."""
-        if self.data.get(session_id):
+        if self.get(session_id):
             raise BackendError("create can't overwrite an existing session")
-
-        self.data[session_id] = data.copy(deep=True)
+        self.put(session_id, data)
 
     async def read(self, session_id: ID):
         """Read an existing session data."""
-        data = self.data.get(session_id)
-        if not data:
-            return
-
-        return data.copy(deep=True)
+        return self.get(session_id)
 
     async def update(self, session_id: ID, data: SessionModel) -> None:
         """Update an existing session."""
-        if self.data.get(session_id):
-            self.data[session_id] = data
-        else:
-            raise BackendError("session does not exist, cannot update")
+        if self.get(session_id):
+            self.put(session_id, data)
+        raise BackendError("session does not exist, cannot update")
 
     async def delete(self, session_id: ID) -> None:
-        """D"""
-        del self.data[session_id]
+        """Delete the session"""
+        return self.table.delete_item(Key={"SessionId": session_id})
